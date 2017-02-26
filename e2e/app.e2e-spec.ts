7d906b8a2fcb888cce070e1639515268fa7fc81e
Seth Davenport <seth@rangle.io>
2017-02-26 18:49:02 -0500

    update e2e tests too

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ExampleAppPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('example-app App', () => {
-  let page: ExampleAppPage;
+describe('Zoo Animals App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ExampleAppPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display message saying "Welcome to the Zoo"', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to the Zoo');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class ExampleAppPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
